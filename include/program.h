d1040c3603e263b70db9902f5d8548a6fb82ba40
myooker <lovasq@yandex.ru>
2026-02-21 23:42:32 +0300

    Add enum list for error messages. Change version to Beta 1.0.1

--- include/program.h
+++ include/program.h
@@ -5,7 +5,7 @@
 
 namespace program {
     namespace fs = std::filesystem;
-    constexpr std::string_view version { "Alpha 1.2.0" };
+    constexpr std::string_view version { "Beta 1.0.1" };
     constexpr std::string_view name { "web-tag-editor" };
 
     enum DIR_DEPTH {
@@ -29,6 +29,41 @@ namespace program {
         fs::path path {};
         std::string extension { path.extension() };
     };
+
+    namespace error {
+        enum MESSAGE {
+            MOUNT_POINT_NOT_EXISTS,
+            FILE_NOT_VALID,
+
+
+            // Flac specified errors
+            NOT_FOUND_XIPH,
+            NOT_FOUND_FIELD_TYPE,
+
+            // Mp3 specified errors
+            NOT_FOUND_ID3V2,
+
+
+            // Mp4 specified errors
+            NOT_FOUND_MP4TAG,
+            UNDEF_ATOM_TYPE,
+
+        };
+
+        consteval std::string_view printMessage(const MESSAGE msg) {
+            switch (msg) {
+                case MOUNT_POINT_NOT_EXISTS:
+                    return "Specified mount-point does not exist. Verify the path and try again.";
+                case FILE_NOT_VALID:
+                    return "Specified file is not valid (cannot open/read).";
+                case NOT_FOUND_XIPH:
+                    return "Specified file does not have Xiph Comments.";
+                default: return "ERROR_MESSAGE";
+            }
+        }
+    }
+
+
 }
 
 #endif //WEB_TAG_EDITOR_PROGRAM_H
