fe780593dbc02889c17d8ae5a24755800225ec45
myooker <lovasq@yandex.ru>
2026-09-13 22:45:00 +0300

    chore(program): remove unused error::MESSAGE enum and printMessage()

--- include/program.h
+++ include/program.h
@@ -75,39 +75,6 @@ namespace program {
             return true;
         }
     };
-
-    namespace error {
-        enum MESSAGE {
-            MOUNT_POINT_NOT_EXISTS,
-            FILE_NOT_VALID,
-
-
-            // Flac specified errors
-            NOT_FOUND_XIPH,
-            NOT_FOUND_FIELD_TYPE,
-
-            // Mp3 specified errors
-            NOT_FOUND_ID3V2,
-
-
-            // Mp4 specified errors
-            NOT_FOUND_MP4TAG,
-            UNDEF_ATOM_TYPE,
-
-        };
-
-        consteval std::string_view printMessage(const MESSAGE msg) {
-            switch (msg) {
-                case MOUNT_POINT_NOT_EXISTS:
-                    return "Specified mount-point does not exist. Verify the path and try again.";
-                case FILE_NOT_VALID:
-                    return "Specified file is not valid (cannot open/read).";
-                case NOT_FOUND_XIPH:
-                    return "Specified file does not have Xiph Comments.";
-                default: return "ERROR_MESSAGE";
-            }
-        }
-    }
 }
 
 #endif //WEB_TAG_EDITOR_PROGRAM_H
